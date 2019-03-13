097a694bc998770a892c12a03af27a13ca6b8cdb
MiDouShiTongTong <i@yyccyy.com>
2019-03-13 14:49:32 +0800

    修改主题颜色

--- front-desk/src/pages/_document.tsx
+++ front-desk/src/pages/_document.tsx
@@ -4,11 +4,17 @@ import Document, { Head, Main, NextScript } from 'next/document';
 export default class _document extends Document {
   public render = (): JSX.Element => {
     return (
-      <html>
+      <html lang="zh-CN">
         <Head>
+          <meta charSet="utf-8"/>
+          <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+          <meta httpEquiv="X-UA-Compatible" content="edge"/>
+          <meta name="theme-color" content="#40a9ff"/>
+          <meta name="keywords" content="新创文化艺术品"/>
+          <meta name="description" content="新文化艺术品"/>
           <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
           <link rel="stylesheet" href="/static/swiper/swiper.min.css"/>
-          <script src="https://cdn.bootcss.com/Swiper/4.4.6/js/swiper.min.js"></script>
+          <script src="https://cdn.bootcss.com/Swiper/4.5.0/js/swiper.min.js"></script>
         </Head>
         <body>
           <Main/>
